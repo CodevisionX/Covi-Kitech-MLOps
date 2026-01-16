997a2d10615cb6ac352df27a7d6994fd3d03b701
Hee-Soo Hong <abihong@codevision.ai>
2026-01-16 11:50:22 +0900

    fix: 환경변수 수정

--- ui/src/environments/environment.docker.ts
+++ ui/src/environments/environment.docker.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // 브라우저가 접근할 주소입니다. 도커 네트워크 내부 이름이 아닌 
   // 실제 외부로 노출된 주소(localhost 또는 서버 IP)를 적어야 합니다.
-  apiUrl: 'http://localhost:8000' //''  // 예: 'http://localhost:8000' 
+  apiUrl: ''  // 예: 'http://localhost:8000' 
 };
